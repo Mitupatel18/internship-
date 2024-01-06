055599ab3410bb5dc54947ac45ec287cf3969bc5
Mitupatel18 <137859840+Mitupatel18@users.noreply.github.com>
2024-01-06 13:12:26 +0530

    Add files via upload

---
+++ djangodb/task/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from task import views
+
+urlpatterns = [
+    path('',views.Task,name='task'),
+    
+]
---
+++ djangodb/task/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Task(request):
+    return render (request,'task-1.html')
